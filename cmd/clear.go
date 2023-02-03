925758d94a0f14c4ff9c9190cf0fb22b4ba143b4
lantian316 <lantian316@163.com>
2023-02-03 16:17:22 +0800

    Update clear.go

--- cmd/clear.go
+++ cmd/clear.go
@@ -28,20 +28,9 @@ func (cl *ClearCommand) Init() {
 
 func (cl *ClearCommand) runClear(cmd *cobra.Command, args []string) error {
 	if len(args) == 0 {
-		if ok, err := clearContext(cfgFile); ok {
-			printString(os.Stdout, fmt.Sprintf("There is nothing to clean in 「%s」\n", cfgFile))
-			return nil
-		} else if err != nil {
-			return err
-		}
+		print("len")
 	} else {
-		for _, file := range args {
-			if ok, err := clearContext(file); ok {
-				printString(os.Stdout, fmt.Sprintf("There is nothing to clean in 「%s」\n", file))
-			} else if err != nil {
-				return err
-			}
-		}
+		print("aaa")
 	}
 	return nil
 }
