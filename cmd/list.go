407c6ce0a50eb832e5ff96598a0b89259d4dc60b
lantian316 <lantian316@163.com>
2023-02-06 12:47:40 +0800

    Update list.go

--- cmd/list.go
+++ cmd/list.go
@@ -18,7 +18,7 @@ func (ll *ListCommand) Init() {
 		Short: "Clear lapsed context, cluster and user",
 		Long:  "Clear lapsed context, cluster and user",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			fmt.Println("赶赶单单啦！")
+			fmt.Println("list赶赶单单啦！")
 			return nil
 		},
 	}
