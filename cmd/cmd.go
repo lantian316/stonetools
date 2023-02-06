7382eee931af38e7f588627b83e6ba9d9f523937
lantian316 <lantian316@163.com>
2023-02-06 10:10:36 +0800

    Create cmd.go

---
+++ cmd/cmd.go
@@ -0,0 +1,18 @@
+package cmd
+
+// NewBaseCommand cmd struct
+func NewBaseCommand() *BaseCommand {
+	cli := NewCli()
+	baseCmd := &BaseCommand{
+		command: cli.rootCmd,
+	}
+	baseCmd.AddCommands(
+		&AddCommand{}, // add command
+
+		&ClearCommand{}, // clear command
+		&ListCommand{}, // clear command
+
+	)
+
+	return baseCmd
+}
