6995248695fe6a08327220319e282f7d691f4a89
lantian316 <lantian316@163.com>
2023-02-06 10:09:19 +0800

    Update list.go

--- cmd/list.go
+++ cmd/list.go
@@ -6,13 +6,21 @@ import (
 	"github.com/spf13/cobra"
 )
 
-var CmdList = &cobra.Command{
-	Use:   "add [string to add]",
-	Short: "Add a new item",
-	Long: `Add is for adding a new item to the list.
-For many years people have added items to lists.`,
-	Args: cobra.MinimumNArgs(1),
-	Run: func(cmd *cobra.Command, args []string) {
-		fmt.Println("Listing: " + args[0])
-	},
+// ListCommand clean command struct
+type LIstCommand struct {
+	BaseCommand
+}
+
+// Init ListCommand
+func (ll *ListCommand) Init() {
+	ll.command = &cobra.Command{
+		Use:   "clear",
+		Short: "Clear lapsed context, cluster and user",
+		Long:  "Clear lapsed context, cluster and user",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			fmt.Println("赶赶单单啦！")
+			return nil
+		},
+	}
+
 }
